7ff365c43a5e7bd35b9307b76eac91f3256ef276
slice3984 <->
2020-07-15 20:51:54 +0200

    who command for a given pickup / sub command

---
+++ commands/sub.ts
@@ -0,0 +1,60 @@
+import { Command } from '../core/types';
+import Util from '../core/util';
+import StatsModel from '../models/stats';
+import PickupModel from '../models/pickup';
+import GuildModel from '../models/guild';
+
+const command: Command = {
+    cmd: 'sub',
+    shortDesc: 'Call for a sub for the latest pickup',
+    desc: 'Call for a sub for the latest pickup',
+    defaults: [
+        {
+            type: 'number', name: 'timeout-after', desc: 'How long the command is callable after the latest pickup',
+            value: 7200000, possibleValues: { from: 1800000, to: 21600000 }
+        }
+    ],
+    global: false,
+    perms: false,
+    exec: async (bot, message, params, defaults) => {
+        const guildSettings = bot.getGuild(message.guild.id);
+
+        const timeUntilNextPromote = guildSettings.lastPromote ?
+            (guildSettings.lastPromote.getTime() + guildSettings.promotionDelay) - new Date().getTime() : null;
+
+        // Can be null if used for the first time
+        if (timeUntilNextPromote && timeUntilNextPromote > 0) {
+            return message.reply(`you can't promote too often, please wait ${Util.formatTime(timeUntilNextPromote)}`);
+        }
+
+        const lastGame = await StatsModel.getLastGame(BigInt(message.guild.id));
+
+        const pickupSettings = await PickupModel.getPickupSettings(BigInt(message.guild.id), lastGame.name);
+
+        let role;
+
+        if (pickupSettings.promotionRole) {
+            role = message.guild.roles.cache.get(pickupSettings.promotionRole.toString());
+
+            if (!role) {
+                return message.reply(`set promotion role for pickup ${lastGame.name} not found`);
+            }
+        } else {
+            return message.reply('no promotion role set for this pickup, can\'t call for a sub');
+        }
+
+        const agoSince = new Date().getTime() - lastGame.startedAt.getTime();
+
+        if (agoSince > defaults[0]) {
+            return message.reply(`can't call a sub for ${lastGame.name}, pickup played too long ago`);
+        }
+
+        guildSettings.updateLastPromote();
+        await GuildModel.updateLastPromote(BigInt(message.guild.id));
+
+        const subMessage = await Util.parseNotifySubMessage(BigInt(message.guild.id), guildSettings.subMessage, pickupSettings);
+        message.channel.send(`${role}, ${subMessage}`);
+    }
+}
+
+module.exports = command;
--- commands/who.ts
+++ commands/who.ts
@@ -8,7 +8,7 @@ const command: Command = {
     shortDesc: 'Show the pickup status of one or multiple pickups',
     desc: 'Show the pickup status of one or multiple pickups',
     args: [
-        { name: '[pickup]...', desc: 'Name of the pickup', required: false }
+        { name: '[pickup]', desc: 'Name of the pickup', required: false }
     ],
     defaults: [
         {
@@ -19,6 +19,8 @@ const command: Command = {
     global: false,
     perms: false,
     exec: async (bot, message, params, defaults) => {
+        const genPickupInfo = pickup => `**${pickup.name}** [ **${pickup.players.length}** / **${pickup.maxPlayers}** ]: ${pickup.players.map(player => `\`${player.nick}\``).join(', ')}`;
+
         if (params.length === 0) {
             const pickups = Array.from((await PickupModel.getActivePickups(BigInt(message.guild.id))).values())
                 .sort((a, b) => b.players.length - a.players.length);
@@ -27,7 +29,6 @@ const command: Command = {
                 return message.channel.send('No active pickups');
             }
 
-            const genPickupInfo = pickup => `**${pickup.name}** [ **${pickup.players.length}** / **${pickup.maxPlayers}** ]: ${pickup.players.map(player => `\`${player.nick}\``).join(', ')}`;
             let msg = '';
             pickups.forEach((pickup, index) => {
                 if (defaults[0] === 'dynamic') {
@@ -50,6 +51,17 @@ const command: Command = {
                 }
             });
             message.channel.send(msg);
+        } else {
+            const pickupName = params[0].toLowerCase();
+
+            const pickup = Array.from((await PickupModel.getActivePickups(BigInt(message.guild.id))).values())
+                .filter(pickup => pickup.name === pickupName);
+
+            if (!pickup.length) {
+                return message.reply(`no results for ${pickupName}`);
+            }
+
+            message.channel.send(genPickupInfo(pickup[0]));
         }
     }
 };
